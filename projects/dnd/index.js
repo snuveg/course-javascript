b991b65b26caa3affaaff041bec45288c8ef188b
Evgeniy Vasilevskiy <snuveg@mail.ru>
2022-08-24 21:35:35 +1000

    tasks 6

--- projects/dnd/index.js
+++ projects/dnd/index.js
@@ -19,9 +19,45 @@ import './dnd.html';
 
 const homeworkContainer = document.querySelector('#app');
 
-document.addEventListener('mousemove', (e) => {});
+document.addEventListener('mousemove', (e) => {
+  const dd = e.target;
+  if (dd.contains('draggable-div')) {
+    // dd.addEventListener - ctrl+c из интернета
+    dd.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
 
-export function createDiv() {}
+      let xStart = evt.clientX;
+      let yStart = evt.clientY;
+      const onMouseMove = function (evtMove) {
+        evtMove.preventDefault();
+
+        const xNew = xStart - evtMove.clientX;
+        const yNew = yStart - evtMove.clientY;
+        xStart = evtMove.clientX;
+        yStart = evtMove.clientY;
+        dd.style.top = dd.offsetTop - yNew + 'px';
+        dd.style.left = dd.offsetLeft - xNew + 'px';
+      };
+      const onMouseUp = function (evtUp) {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
+  }
+});
+
+export function createDiv() {
+  const div = document.createElement('div');
+  div.classList.add('draggable-div');
+  div.style.background = 'red';
+  div.style.top = '10px';
+  div.style.left = '20px';
+  div.style.width = '30px';
+  div.style.height = '40px';
+  return div;
+}
 
 const addDivButton = homeworkContainer.querySelector('#addDiv');
 
